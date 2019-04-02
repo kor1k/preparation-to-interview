1a3516981823ae811ea9df622148d22dba40d434
kor1k <koortttt@gmail.com>
2019-04-02 17:16:42 +0300

    positions, box sizing, flex, grid ____ arrays -> forEach, filter

---
+++ src/script.js
@@ -0,0 +1,32 @@
+// --------------------------------------------------------forEach start------------------------------------------------------------------//
+
+// function forEachExample() {
+//     var arr = ['Mercedes', 'Opel', 'BMW', 'Mazda', 'Śkoda'];
+//     arr.forEach(function (item, i, arr) {
+//         console.log(i + ': ' + item + "(massive: " + arr + ')');
+//     });
+// }
+//
+// forEachExample();
+
+// --------------------------------------------------------forEach end------------------------------------------------------------------//
+
+// --------------------------------------------------------filter start------------------------------------------------------------------//
+
+// function filterExample() {
+//     var arr = [11, -112, 200, 500, 0, 19, 10, -135];
+//     var highNumber = arr.filter(function (number) {
+//         return number < 0;
+//     });
+//     var lowNumber = arr.filter(function (number2) {
+//         return number2 > 0;
+//     });
+//     alert(highNumber);
+//     alert(lowNumber);
+// }
+//
+// filterExample();
+
+// --------------------------------------------------------filter end------------------------------------------------------------------//
+
+

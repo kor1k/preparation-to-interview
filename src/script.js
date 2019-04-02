bc5ea27c0b8523fa2c946bcf5aa038adc05c680a
kor1k <koortttt@gmail.com>
2019-04-02 18:19:26 +0300

    js -> map (simple example of length words in arr, multiply Elements in arr, power of Elements in arr.

--- src/script.js
+++ src/script.js
@@ -29,4 +29,37 @@
 
 // --------------------------------------------------------filter end------------------------------------------------------------------//
 
+// --------------------------------------------------------map start------------------------------------------------------------------//
+
+
+// function mapExample() {
+//     var arrNames = ['html', 'css', 'javascript', 'react.js', 'java', 'angular', 'c++'];
+//
+//     var namesLengths = arrNames.map(function (name) {
+//         return name.length;
+//     });
+//     alert(namesLengths)
+// }
+//
+// mapExample();
+
+// function multiplyOfNumbersMapExample() {
+//     var numbers = [10, 20, 30, 40, 50];
+//     var multiply = numbers.map(x => x * 2);
+//     alert(multiply);
+// }
+//
+// multiplyOfNumbersMapExample();
+
+// function powerOfNumbersMapExample() {
+//     var numbers = [10, 20, 30, 40, 50];
+//     var multiply = numbers.map(x => x * x);
+//     alert(multiply);
+// }
+//
+// powerOfNumbersMapExample();
+// --------------------------------------------------------map end------------------------------------------------------------------//
+
+// --------------------------------------------------------map end------------------------------------------------------------------//
+
 

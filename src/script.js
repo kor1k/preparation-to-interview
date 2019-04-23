1f1e0711df595760ab67f370f51d3d4ee3a7f681
kor1k <koortttt@gmail.com>
2019-04-23 10:26:11 +0300

    added some new things

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
-boat.onclick = function() {
+
+boat.onclick = function () {
 
     this.onclick = null; // только первый клик сработает
 
@@ -17,7 +18,7 @@ boat.onclick = function() {
 
     go();
 
-    boat.addEventListener('transitionend', function() {
+    boat.addEventListener('transitionend', function () {
         times++;
         go();
     });
@@ -696,7 +697,7 @@ color.onclick = function () {
     this.style.width = '200px';
     this.style.height = '120px';
     this.style.color = 'black';
-}
+};
 
 // var seclectedTag;
 //
@@ -713,8 +714,40 @@ color.onclick = function () {
 //     selectedTd.classList.add('highlight')
 // }
 
-stripe.onclick = function() {
+stripe.onclick = function () {
     stripe.classList.add('animate');
 };
 
 // __________animations end____________
+
+
+// alert( location.href );
+var sBrowser, sUsrAg = navigator.userAgent;
+
+// The order matters here, and this may report false positives for unlisted browsers.
+
+if (sUsrAg.indexOf("Firefox") > -1) {
+    sBrowser = "Mozilla Firefox";
+    // "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
+} else if (sUsrAg.indexOf("Opera") > -1 || sUsrAg.indexOf("OPR") > -1) {
+    sBrowser = "Opera";
+    //"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.106"
+} else if (sUsrAg.indexOf("Trident") > -1) {
+    sBrowser = "Microsoft Internet Explorer";
+    // "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; Zoom 3.6.0; wbx 1.0.0; rv:11.0) like Gecko"
+} else if (sUsrAg.indexOf("Edge") > -1) {
+    sBrowser = "Microsoft Edge";
+    // "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299"
+} else if (sUsrAg.indexOf("Chrome") > -1) {
+    sBrowser = "Google Chrome or Chromium";
+    // "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/66.0.3359.181 Chrome/66.0.3359.181 Safari/537.36"
+} else if (sUsrAg.indexOf("Safari") > -1) {
+    sBrowser = "Apple Safari";
+    // "Mozilla/5.0 (iPhone; CPU iPhone OS 11_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.0 Mobile/15E148 Safari/604.1 980x1306"
+} else {
+    sBrowser = "unknown";
+}
+
+// alert("You are using: " + sBrowser);
+
+// alert(navigator.platform);
